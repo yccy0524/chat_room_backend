46cbede653dceab8526c13804753d1b31e44925e
yancheng <404174228@qq.com>
2024-08-30 09:14:59 +0800

    fix: 🐛 修改好友列表

--- src/friendship/friendship.controller.ts
+++ src/friendship/friendship.controller.ts
@@ -2,7 +2,7 @@
  * @Author: yancheng 404174228@qq.com
  * @Date: 2024-08-26 10:28:58
  * @LastEditors: yancheng 404174228@qq.com
- * @LastEditTime: 2024-08-27 09:06:12
+ * @LastEditTime: 2024-08-30 09:14:25
  * @Description:
  *
  */
@@ -43,5 +43,22 @@ export class FriendshipController {
       throw new HttpException('好友id不能为空', HttpStatus.BAD_REQUEST);
     }
     return await this.friendshipService.agree(friendId, uid);
+  }
+
+  // 拒绝添加好友
+  @RequireLogin()
+  @Get('reject/:uid')
+  async reject(@Param('uid') friendId: number, @UserInfo('uid') uid: number) {
+    if (!friendId) {
+      throw new HttpException('好友id不能为空', HttpStatus.BAD_REQUEST);
+    }
+    return await this.friendshipService.reject(friendId, uid);
+  }
+
+  // 获取朋友列表
+  @RequireLogin()
+  @Get('friendship')
+  async getFriendShip(@UserInfo('uid') uid: number) {
+    return await this.friendshipService.getFriendShip(uid);
   }
 }
--- src/friendship/friendship.service.ts
+++ src/friendship/friendship.service.ts
@@ -1,3 +1,10 @@
+/*
+ * @Author: yancheng 404174228@qq.com
+ * @Date: 2024-08-26 10:28:58
+ * @LastEditors: yancheng 404174228@qq.com
+ * @LastEditTime: 2024-08-30 09:13:57
+ * @Description:
+ */
 import { Inject, Injectable } from '@nestjs/common';
 import { AddFriendDto } from './dto/add-friend.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
@@ -55,5 +62,65 @@ export class FriendshipService {
       });
     }
     return '添加成功';
+  }
+
+  // 拒绝添加好友
+  async reject(friendId: number, uid: number) {
+    await this.prisma.friend_Request.updateMany({
+      where: {
+        toUid: uid,
+        fromUid: friendId,
+        status: 0,
+      },
+      data: {
+        status: 2,
+      },
+    });
+
+    return '已拒绝';
+  }
+
+  // 获取好友列表
+  async getFriendShip(uid: number) {
+    const friends = await this.prisma.friendShip.findMany({
+      where: {
+        OR: [
+          {
+            uid: uid,
+          },
+          {
+            friendId: uid,
+          },
+        ],
+      },
+    });
+
+    const set = new Set<number>();
+    friends.forEach((item) => {
+      set.add(item.uid);
+      set.add(item.friendId);
+    });
+
+    const uidList = Array.from([...set]).filter((item) => item !== uid);
+
+    const res = [];
+    for (let i = 0; i < uidList.length; i++) {
+      const user = await this.prisma.user.findUnique({
+        where: {
+          id: uidList[i],
+        },
+        select: {
+          id: true,
+          username: true,
+          nickname: true,
+          headePic: true,
+          email: true,
+        },
+      });
+
+      res.push(user);
+    }
+
+    return res;
   }
 }
