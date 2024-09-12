d4cf269d4b139ec73036ef728f1464c752d3fb3b
yancheng <404174228@qq.com>
2024-09-12 12:00:01 +0800

    feat: 🎸 dockerfile

--- src/redis/redis.module.ts
+++ src/redis/redis.module.ts
@@ -8,20 +8,21 @@
 import { Global, Module } from '@nestjs/common';
 import { RedisService } from './redis.service';
 import { createClient } from 'redis';
-
+import { ConfigService } from '@nestjs/config';
 @Global()
 @Module({
   providers: [
     RedisService,
     {
       provide: 'REDIS_CLIENT',
-      async useFactory() {
+      inject: [ConfigService],
+      async useFactory(configService: ConfigService) {
         const client = createClient({
           socket: {
-            host: 'localhost',
-            port: 6379,
+            host: configService.get('REDIS_HOST'),
+            port: configService.get('REDIS_PORT'),
           },
-          database: 2,
+          database: configService.get('REDIS_DATABASE'),
         });
         await client.connect();
         return client;
